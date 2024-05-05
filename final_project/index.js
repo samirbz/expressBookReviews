146b6979898a96f7ee9e11c4b238b91a3f2c3c30
samir <febsec14@gmail.com>
2024-05-05 21:57:49 +0545

    authentication & task 1 completed

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,23 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    console.log(req.session)
+    //Write the authenication mechanism here
+    if (req.session.views) {
+        req.session.views++;
+        res.send(`You have visited this page ${req.session.views} times`);
+    } else {
+        req.session.views = 1;
+        res.send('Welcome to the site');
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
