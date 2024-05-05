5a9b0ceb6e91897b37c3752b8a8007fe4bb553a4
samir <febsec14@gmail.com>
2024-05-05 21:59:52 +0545

    Task 2 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,7 +30,13 @@ public_users.get('/', function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    res.send(book);
+  } else {
+    res.status(404).send("Book not found");
+  }
 });
 
 // Get book details based on author
